f6e719cbf6f3b75af664b861ec040aa05d9dd128
sinjed <oooguuh@gmail.com>
2025-09-07 15:50:21 +0200

    interface
    Signed-off-by: sinjed <oooguuh@gmail.com>

--- ugc_cinema_api/regions.py
+++ ugc_cinema_api/regions.py
@@ -1,11 +1,15 @@
 from ugc_cinema_api._core.regions.get_regions import *
+from ugc_cinema_api._core.cinema.get_cinemas_from_region import *
+from ugc_cinema_api.types import Region
 import json
 
 
 class Regions:
 
 	def __init__(self):
-		self.regions = GetRegions().regions
+		self.regions = self.get_regions()
 	
 	def __repr__(self):
 		return json.dumps(self.regions, indent=4)
+
+	def get_regions(self) -> list[Region]: return GetRegions().regions
